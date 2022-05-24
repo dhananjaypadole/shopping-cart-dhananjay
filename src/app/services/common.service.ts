80959cb2650b4fcab78093f79cbea0c976bb93f2
Khushbu Patel <52137772+khushi9999@users.noreply.github.com>
2022-05-24 22:09:33 +0530

    added server and api

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -5,5 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class CommonService {
   assetsFolderPath = '../../../assets';
+  // assetsFolderPath = 'http://localhost:6006';
+
+  categoriesApi = 'http://localhost:6002/categories';
+  bannerApi = 'http://localhost:6001/banner';
+  productsApi = 'http://localhost:6004/products';
+  cartApi = 'http://localhost:6003/cart';
+  userApi = 'http://localhost:6005/users';
+
   constructor() { }
 }
